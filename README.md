3b9781802273348915dce8b0fe857e4e6da4643e
Paul Bailey <paul@neutron.studio>
2022-02-08 10:29:38 -0600

    better bare command usage

--- README.md
+++ README.md
@@ -29,6 +29,10 @@ pdm-django inherits from the `pdm run` command, so if you would like to load a d
 _.env_file = ".env"
 ```
 
+## CLI Option Conflicts
+
+You may run into some options conflicts between the `pdm` command and the Django command you run. As of right now the only one I can find is `pdm manage --help` for which you will get the pdm help instead of the Django help. However, note you can still run `pdm manage` or `pdm manage help` to view the Django help. Same situation applies for `pdm django-admin --help`.
+
 ## Shameless Plugs
 
 I built this library originally for the [NeutronSync Service](https://www.neutronsync.com/). So if you would like to support this project please support the service with a subscription to NeutronSync or a [donation](https://github.com/sponsors/neutron-sync) to the open source libraries.
--- pdm_django/main.py
+++ pdm_django/main.py
@@ -2,26 +2,48 @@ import argparse
 import subprocess
 import sys
 
+from pdm.cli.utils import PdmFormatter
 from pdm.cli.commands.base import BaseCommand
 from pdm.cli.commands.run import Command as RunCommand
 
 
-class ManageCommand(RunCommand):
+class DjangoRunCommand(RunCommand):
+  OPTIONS = []
+  COMMAND_PREFIX = []
+
+  def add_arguments(self, parser):
+    parser.add_argument(
+      "args",
+      nargs=argparse.REMAINDER,
+      help="Arguments that will be passed to the command",
+    )
+
+  def handle(self, project, options):
+    options.list = False
+    options.site_packages = False
+    options.command = 'django'
+    super().handle(project, options)
+
+  @classmethod
+  def _run_command(cls, project, args, **kwargs):
+    # remove dummy command
+    args.pop(0)
+
+    # add real command
+    args = cls.COMMAND_PREFIX + args
+    return RunCommand._run_command(project, args, **kwargs)
+
+
+class ManageCommand(DjangoRunCommand):
   """shortcut for pdm run python manage.py"""
 
-  @staticmethod
-  def _run_command(project, args, **kwargs):
-    args = ['python', 'manage.py'] + args
-    return RunCommand._run_command(project, args, **kwargs)
+  COMMAND_PREFIX = ['python', 'manage.py']
 
 
-class AdminCommand(RunCommand):
+class AdminCommand(DjangoRunCommand):
   """shortcut for pdm run django-admin"""
 
-  @staticmethod
-  def _run_command(project, args, **kwargs):
-    args = ['django-admin'] + args
-    return RunCommand._run_command(project, args, **kwargs)
+  COMMAND_PREFIX = ['django-admin']
 
 
 def reg_commands(core):
